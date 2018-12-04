3a835925206e16b701a131e32e877dc82d5400e5
Cory Dudart <corydudart@gmail.com>
2018-12-04 14:17:48 -0700

    admin layout and pages

---
+++ layouts/admin.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+
+export default {
+  components:{
+    TheHeader,
+    TheSidenav,
+  },
+  data(){
+    return {
+      displaySidenav: false
+    }
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: "Open Sans", sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
